c98dd86647b729065718dc30ee6d0746fd854e8c
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-08-25 15:42:59 -0300

    create register screen

---
+++ src/screens/Register/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Dimensions, PixelRatio } from "react-native";
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+import { colors } from "../../styles";
+
+const { width, height } = Dimensions.get("window");
+const scale = width / 320;
+
+export function normalize(size) {
+  const newSize = size * scale;
+  return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.purpleLight,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
