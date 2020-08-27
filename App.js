e221e633a2039863c8f23ea0a0de8e90f4d229d2
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-08-27 15:54:37 -0300

    add theme to status bar

--- App.js
+++ App.js
@@ -15,8 +15,8 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" backgroundColor={"white"} />
         <Navigation colorScheme={colorScheme} />
-        <StatusBar />
       </SafeAreaProvider>
     );
   }
