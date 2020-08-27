2da6a7416dff45d5aa8607eb5d5d159857880dbf
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-08-27 15:52:08 -0300

    add onPress

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -24,8 +24,12 @@ import { normalize } from "../../components/TextInput/styles";
 const { height } = Dimensions.get("window");
 const ITEM_HEIGHT = height * 0.25;
 
-export default function Home() {
+export default function Home({ navigation, route }) {
   const [services, SetServices] = React.useState([
+    {
+      id: 0,
+      title: "Todos",
+    },
     {
       id: 1,
       title: "Banho",
@@ -46,11 +50,16 @@ export default function Home() {
       id: 5,
       title: "Tosa verão",
     },
-    // {
-    //   id: 6,
-    //   title: "Banho e corte de unhas",
-    // },
+    {
+      id: 6,
+      title: "Banho e corte de unhas",
+    },
+    {
+      id: 7,
+      title: "Banho e corte de unhas",
+    },
   ]);
+  const [selectedService, setSelectedService] = React.useState(0);
   const [petShop, setPetShop] = React.useState([
     {
       id: 1,
@@ -59,6 +68,7 @@ export default function Home() {
       city: "Londrina",
       openTime: "Horário de atendimento das 08 as 19hrs",
       rating: 5,
+      imgUrl: require("../../../assets/images/dog_shower.jpg"),
     },
     {
       id: 2,
@@ -67,6 +77,7 @@ export default function Home() {
       city: "Londrina",
       openTime: "Horário de atendimento das 08 as 19hrs",
       rating: 0,
+      imgUrl: require("../../../assets/images/dog_shower.jpg"),
     },
     {
       id: 3,
@@ -75,22 +86,25 @@ export default function Home() {
       city: "Londrina",
       openTime: "Horário de atendimento das 08 as 19hrs",
       rating: 2,
+      imgUrl: require("../../../assets/images/dog_shower.jpg"),
     },
     {
       id: 4,
-      name: "",
+      name: "Pet shop doguinho",
       address: "Avenida Higienopolis, 10",
       city: "Londrina",
       openTime: "Horário de atendimento das 08 as 19hrs",
       rating: 3,
+      imgUrl: require("../../../assets/images/dog_shower.jpg"),
     },
     {
       id: 5,
-      name: "",
+      name: "Pet do Gregs",
       address: "Avenida Higienopolis, 10",
       city: "Londrina",
       openTime: "Horário de atendimento das 08 as 19hrs",
       rating: 1,
+      imgUrl: require("../../../assets/images/dog_shower.jpg"),
     },
   ]);
   const [search, setSearch] = React.useState("");
@@ -101,57 +115,94 @@ export default function Home() {
   const renderServices = (item) => {
     return (
       <View style={{}}>
-        <TouchableOpacity style={styles.serviceItemView}>
-          <Ionicons name="ios-cut" size={26} color={colors.greyLight} />
-          <Text style={styles.serviceText}>{item[0].title}</Text>
+        <TouchableOpacity
+          style={
+            item[0].id != selectedService
+              ? styles.serviceItemView
+              : styles.serviceItemSelectedView
+          }
+          onPress={() => setSelectedService(item[0].id)}
+        >
+          {item[0].id != 0 ? (
+            <Ionicons name="ios-cut" size={26} color={colors.greyLight} />
+          ) : null}
+          <Text
+            style={
+              item[0].id != selectedService
+                ? styles.serviceText
+                : styles.serviceSelectedText
+            }
+          >
+            {item[0].title}
+          </Text>
         </TouchableOpacity>
 
         {item.length > 1 ? (
-          <TouchableOpacity style={styles.serviceItemView}>
+          <TouchableOpacity
+            style={
+              item[1].id != selectedService
+                ? styles.serviceItemView
+                : styles.serviceItemSelectedView
+            }
+            onPress={() => setSelectedService(item[1].id)}
+          >
             <Ionicons name="ios-cut" size={24} color={colors.greyLight} />
-            <Text style={styles.serviceText}>{item[1].title}</Text>
+            <Text
+              style={
+                item[1].id != selectedService
+                  ? styles.serviceText
+                  : styles.serviceSelectedText
+              }
+            >
+              {item[1].title}
+            </Text>
           </TouchableOpacity>
         ) : null}
       </View>
     );
   };
 
-  const renderItem = (item) => {
+  const renderPetShop = (item) => {
     return (
       <View style={{}}>
-        <TouchableOpacity style={styles.petShopItemView}>
-          <View
-            style={{
-              // backgroundColor: colors.blue,
-              flexDirection: "row",
-              flex: 1,
-              width: wp(90),
-              alignItems: "center",
-              // height: wp(30),
-            }}
-          >
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Establishment", {
+              id: item.id,
+              name: item.name,
+              address: item.address,
+              city: item.city,
+              openTime: item.openTime,
+              rating: item.rating,
+              imgUrl: item.imgUrl,
+            });
+          }}
+          style={styles.petShopItemView}
+        >
+          <View style={styles.petShopItemImageView}>
             <Image
-              source={require("../../../assets/images/dog_shower.jpg")}
+              source={item.imgUrl}
               style={{
-                width: wp(40),
-                height: wp(50),
-                flex: 1,
+                width: wp(35),
+                height: wp(30),
+                // flex: 1,
                 alignSelf: "center",
               }}
-              resizeMode="contain"
+              resizeMode="stretch"
             />
             <View
               style={{
+                marginLeft: wp(2),
                 // backgroundColor: colors.green,
                 flex: 1,
-                alignItems: "flex-start",
+                alignItems: "center",
               }}
             >
-              <Text style={styles.petShopText}>{item.name}</Text>
-              <Text>{item.address}</Text>
-              <Text>{item.city}</Text>
-              <Text>{item.openTime}</Text>
-              <Rating rate={item.rating}></Rating>
+              <Text style={styles.petShopName}>{item.name}</Text>
+              <Text style={styles.petShopInfos}>{item.address}</Text>
+              <Text style={styles.petShopInfos}>{item.city}</Text>
+              <Text style={styles.petShopInfos}>{item.openTime}</Text>
+              <Rating rate={item.rating} />
             </View>
           </View>
         </TouchableOpacity>
@@ -171,6 +222,16 @@ export default function Home() {
       <FlatList
         ListHeaderComponent={
           <>
+            {/* <Image
+              source={require("../../../assets/images/wall.png")}
+              style={{
+                width: wp(100),
+                height: wp(30),
+                marginBottom: wp(-11),
+                // alignSelf: "center",
+              }}
+              resizeMode="contain"
+            /> */}
             <View style={styles.textInputView}>
               <TInput
                 icon="ios-search"
@@ -199,26 +260,32 @@ export default function Home() {
         getItemLayout={getItemLayout}
         ListFooterComponent={
           <>
-            <Text style={styles.servicesTitle}>Serviços</Text>
-            <FlatList
-              data={arrays}
-              keyExtractor={keyExtractor}
-              horizontal={true}
-              renderItem={({ item, index }) => renderServices(item)}
-              showsVerticalScrollIndicator={false}
-              showsHorizontalScrollIndicator={false}
-              getItemLayout={getItemLayout}
-            />
+            <View style={styles.sectionContainer}>
+              <Text style={styles.servicesTitle}>Serviços</Text>
+              <FlatList
+                data={arrays}
+                keyExtractor={keyExtractor}
+                horizontal={true}
+                renderItem={({ item, index }) => renderServices(item)}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                getItemLayout={getItemLayout}
+              />
+            </View>
+
             <View style={styles.separator} />
-            <Text style={styles.servicesTitle}>Pet shops</Text>
-            <FlatList
-              data={petShop}
-              keyExtractor={keyExtractor}
-              renderItem={({ item, index }) => renderItem(item)}
-              showsVerticalScrollIndicator={false}
-              showsHorizontalScrollIndicator={false}
-              getItemLayout={getItemLayout}
-            />
+
+            <View style={styles.sectionContainer}>
+              <Text style={styles.servicesTitle}>Pet shops</Text>
+              <FlatList
+                data={petShop}
+                keyExtractor={keyExtractor}
+                renderItem={({ item, index }) => renderPetShop(item)}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                getItemLayout={getItemLayout}
+              />
+            </View>
           </>
         }
       />
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,19 +1,11 @@
-import { StyleSheet, Dimensions, PixelRatio } from "react-native";
+import { StyleSheet } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-import { colors } from "../../styles";
-
-const { width, height } = Dimensions.get("window");
-const scale = width / 320;
-
-export function normalize(size) {
-  const newSize = size * scale;
-  return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
-}
+import { colors, fontFamily, metrics } from "../../styles";
 
 export const styles = StyleSheet.create({
   container: {
@@ -24,7 +16,7 @@ export const styles = StyleSheet.create({
   textInputView: {
     // backgroundColor: colors.green1,
     borderColor: colors.purpleDarker,
-    borderRadius: 20,
+    borderRadius: metrics.borderRadius,
     borderWidth: 2,
     width: wp(90),
     alignSelf: "center",
@@ -37,48 +29,85 @@ export const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  sectionContainer: {
+    // backgroundColor: colors.pink,
+    width: wp(90),
+    alignSelf: "center",
+  },
   servicesTitle: {
     color: colors.purpleDarker,
-    fontSize: normalize(20),
-    marginLeft: wp(5),
-    fontFamily: "notoSans-bold",
+    fontSize: metrics.fontSize20,
+    fontFamily: fontFamily.notoSans_bold,
   },
   serviceItemView: {
     backgroundColor: colors.purpleLight,
     width: wp(28),
     height: wp(30),
     marginBottom: wp(4),
-    borderRadius: 20,
+    borderRadius: metrics.borderRadius,
     alignItems: "center",
     justifyContent: "center",
     elevation: 3,
-    marginLeft: wp(5),
+    marginRight: wp(5),
+  },
+  serviceItemSelectedView: {
+    backgroundColor: colors.purple,
+    width: wp(28),
+    height: wp(30),
+    marginBottom: wp(4),
+    borderRadius: metrics.borderRadius,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 3,
+    marginRight: wp(5),
   },
   serviceText: {
     textAlign: "center",
     color: colors.purpleDarker,
-    fontSize: normalize(15),
-    fontFamily: "notoSans-bold",
+    fontSize: metrics.fontSize15,
+    fontFamily: fontFamily.notoSans_bold,
+  },
+  serviceSelectedText: {
+    textAlign: "center",
+    color: colors.white,
+    fontSize: metrics.fontSize15,
+    fontFamily: fontFamily.notoSans_bold,
   },
   petShopItemView: {
+    paddingVertical: 5,
     // flexDirection: "row",
     flex: 1,
     backgroundColor: colors.white,
     width: wp(90),
     // height: wp(30),
     marginBottom: wp(4),
-    borderRadius: 20,
-    borderColor: colors.purple,
-    borderWidth: 1,
+    borderRadius: metrics.borderRadius,
+    // borderColor: colors.purple,
+    // borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
-    elevation: 3,
-    marginLeft: wp(5),
+    elevation: 5,
   },
-  petShopText: {
-    textAlign: "left",
+  petShopItemImageView: {
+    // backgroundColor: colors.blue,
+    flexDirection: "row",
+    flex: 1,
+    width: wp(90),
+    alignItems: "center",
+    // height: wp(30),
+  },
+  petShopName: {
+    alignSelf: "flex-start",
+
     color: colors.purpleDarker,
-    fontSize: normalize(15),
-    fontFamily: "notoSans-bold",
+    fontSize: metrics.fontSize15,
+    fontFamily: fontFamily.notoSans_bold,
+  },
+  petShopInfos: {
+    alignSelf: "flex-start",
+    fontSize: metrics.fontSize11,
+    fontFamily: fontFamily.notoSans_regular,
+    color: colors.greyDark,
+    marginVertical: 1,
   },
 });
