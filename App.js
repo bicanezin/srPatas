33327455810b5bdab744aa652be2bb235e3a1dda
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-08-28 15:47:11 -0300

    add lib sentry

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Sentry from "sentry-expo";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
@@ -9,6 +10,13 @@ import Navigation from "./src/navigation";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  Sentry.init({
+    dsn:
+      "https://255c4d4ec5fb49acaf88d985154dab7b@o440198.ingest.sentry.io/5408391",
+    enableInExpoDevelopment: true,
+    debug: true,
+  });
 
   if (!isLoadingComplete) {
     return null;
